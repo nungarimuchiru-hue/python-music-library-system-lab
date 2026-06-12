af9fd1890d52fc02f181d0509bb71e4e6665ee93
nungarimuchiru-hue <nungari.muchiru@student.moringaschool.com>
2026-06-12 14:20:25 +0300

    Add changes made to song.py

--- lib/song.py
+++ lib/song.py
@@ -2,21 +2,20 @@ class Song:
     total_songs = 0
     unique_artists = set()
     unique_genres = set()
-    genre_count = {}
-    artist_count = {}
+    genre_count = {} # Added 's' to match the constructor usage
+    artist_count = {} # Added 's' to match the constructor usage
 
     def __init__(self,title,artist,genre):
         self.title = title
         self.artist = artist
         self.genre = genre
 
-        song.total_songs += 1
+        Song.total_songs += 1
+        Song.unique_artists.add(self.artist)
+        Song.unique_genres.add(self.genre)
 
-        song.unique_artists.add(self.artist)
-        song.unique_genres.add(self.genre)
-
-        song.genre_counts[self.genre] = song.genre_counts.get(self.genre, 0) + 1
-        song.artist_counts[self.artist] = song.artist_counts.get(self.artist, 0) + 1
+        Song.genre_counts[self.genre] = Song.genre_counts.get(self.genre, 0) + 1
+        Song.artist_counts[self.artist] = Song.artist_counts.get(self.artist, 0) + 1
 
         @classmethod
         def get_library_stats(cls):
